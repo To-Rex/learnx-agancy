5f8af1d7d7f1619b8424140415b7f0eeb21c67cc
Dilshodjon <89077434+To-Rex@users.noreply.github.com>
2025-07-30 17:38:49 +0500

    Admin parolni oddiy text sifatida saqlash

---
+++ supabase/migrations/20250730123844_late_mode.sql
@@ -0,0 +1,6 @@
+@@ .. @@
+ INSERT INTO admin_users (username, password_hash, role) VALUES
+-('admin', '$2a$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'super_admin');
++('admin', 'admin', 'super_admin');
+ 
+ -- Add other default admin users
